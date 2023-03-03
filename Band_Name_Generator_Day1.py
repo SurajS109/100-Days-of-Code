400678a73f2aa23e764ffdc3f6118085d3f88c0f
Suraj S <103848005+SurajS109@users.noreply.github.com>
2023-03-03 12:02:14 +0530

    Add files via upload

---
+++ Band_Name_Generator_Day1.py
@@ -0,0 +1,4 @@
+print('Hello, welcome to the brand name generator.')
+city = input('which city did you grow up in? \n')
+pet_name = input('what is the name of your pet? \n')
+print('Your brand name could be', city , pet_name +'!')
